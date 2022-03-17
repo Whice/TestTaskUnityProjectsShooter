4d45ec30a26964769839f738eebf05539b0eb2c7
A.Baranov <a.baranov@overmindgroup.com>
2022-03-17 12:55:25 +0300

    Корректировка настроек освещения. ХП босса оставлена в покое.

--- Assets/Scripts/Game/Camera/CameraView.cs
+++ Assets/Scripts/Game/Camera/CameraView.cs
@@ -30,12 +30,6 @@ public class CameraView : ItemView
 
     #endregion Main camera.
 
-
-    private void Awake()
-    {
-        this.centerOfCanvasBossHPBarX = this.canvasBossHPBarRectTransform.rect.width / 2 - 
-            this.bossHPBar.GetComponent<RectTransform>().rect.width/2;
-    }
     #region полоска HP босса.
 
     [Header("Boss HP bar")]
@@ -48,69 +42,6 @@ public class CameraView : ItemView
     /// Вся полоска ХП босса.
     /// </summary>
     public GameObject bossHPBar = null;
-    /// <summary>
-    /// Данные о холсте ХПшки босса.
-    /// </summary>
-    [SerializeField]
-    private RectTransform canvasBossHPBarRectTransform = null;
-    /// <summary>
-    /// Изначальное положение ХПшки босса по оси Х.
-    /// </summary>
-    private Single centerOfCanvasBossHPBarX;
-
-    private GameObject bossField = null;
-    private GameObject boss
-    {
-        get
-        {
-            if(this.bossField==null)
-            {
-                this.bossField = ArenaModel.instance.boss;
-            }
-            return this.bossField;
-        }
-    }
-
-    private void Update()
-    {
-        if (this.boss.activeSelf)
-        {
-            Vector3 viewPosition = this.mainCamera.WorldToViewportPoint(this.boss.transform.position);
-            if (InViewportCamera(this.boss.transform.position))
-            {
-                this.bossHPBar.transform.position = Vector3.Lerp
-                    (
-                    this.bossHPBar.transform.position,
-                    new Vector3(
-                                viewPosition.x,
-                                this.bossHPBar.transform.position.y,
-                                this.bossHPBar.transform.position.z
-                                ),
-                    Time.deltaTime
-                    );
-            }
-            else
-            {
-                this.bossHPBar.transform.position = Vector3.Lerp
-                    (
-                    this.bossHPBar.transform.position,
-                    new Vector3(
-                                this.centerOfCanvasBossHPBarX +this.bossHPBar.GetComponent<RectTransform>().rect.width/2,
-                                this.bossHPBar.transform.position.y,
-                                this.bossHPBar.transform.position.z
-                                ),
-                    Time.deltaTime
-                    );
-            }
-
-            Debug.Log(
-                nameof(viewPosition) + " - " +
-                nameof(viewPosition.x) + ": " + viewPosition.x.ToString() + " " +
-                nameof(viewPosition.y) + ": " + viewPosition.y.ToString() + " " +
-                nameof(viewPosition.z) + ": " + viewPosition.z.ToString() + " " 
-                );
-        }
-    }
 
     #endregion полоска HP босса.
 }
